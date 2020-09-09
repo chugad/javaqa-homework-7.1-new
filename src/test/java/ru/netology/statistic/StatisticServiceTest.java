564176c3f9488a75af1f8794d206536b1c3134a1
Oleg Dudin <o.v.dudin@yandex.ru>
2020-09-09 22:10:00 +0700

    Initial commit

---
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticService service = new StatisticService();
+
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomes);
+
+        assertEquals(expected, actual);
+    }
+}
